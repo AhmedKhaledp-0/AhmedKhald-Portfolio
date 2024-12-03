bb43d5473409314fd68e7dc543b6d3087c72810c
AhmedKhaledp-0 <brokeninfp@gmail.com>
2024-12-03 14:53:53 +0200

    Handling some errors for the Deployment

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icons } from "../../utils/icons";
 import { projects } from "../../utils/constants";
