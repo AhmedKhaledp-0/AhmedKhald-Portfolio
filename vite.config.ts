6c71b3e72cb92d15eace88d9a84d3ae6d6f8eca8
AhmedKhaledp-0 <brokeninfp@gmail.com>
2025-06-11 10:21:23 +0300

    Refactor CSS and Vite configuration: enhance performance optimizations and improve chunking strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',
-    minify: 'terser',
+    target: "esnext",
+    minify: "terser",
+    cssMinify: true,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          icons: ['@fortawesome/react-fontawesome', '@fortawesome/fontawesome-svg-core', 'react-icons'],
-          ui: ['@reduxjs/toolkit', 'react-redux']
-        }
-      }
+          vendor: ["react", "react-dom"],
+          icons: [
+            "@fortawesome/react-fontawesome",
+            "@fortawesome/fontawesome-svg-core",
+          ],
+          reactIcons: ["react-icons/si", "react-icons/fa"],
+          ui: ["@reduxjs/toolkit", "react-redux"],
+          utils: ["formik", "yup"],
+        },
+      },
     },
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
-      }
-    }
+        drop_debugger: true,
+        pure_funcs: [
+          "console.log",
+          "console.info",
+          "console.debug",
+          "console.warn",
+        ],
+      },
+    },
+    chunkSizeWarningLimit: 1000,
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', '@fortawesome/react-fontawesome']
-  }
-})
+    include: ["react", "react-dom", "@fortawesome/react-fontawesome"],
+    exclude: [
+      "@fortawesome/free-brands-svg-icons",
+      "@fortawesome/free-solid-svg-icons",
+    ],
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+});
